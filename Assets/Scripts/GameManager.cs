fed60e2577c5eda90d395192b79a1d2ca22cd8e4
Daniel Torchia <daniel.torchia.99@gmail.com>
2025-01-28 22:47:38 +0100

    Code and files cleanup; GameManager singleton object created

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("GameManager non trovato nella scena!");
+            }
+            return _instance;
+        }
+    }
+
+    public GameObject player; 
+    public UIManager uiManager;
+    public AudioManager audioManager;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+}
